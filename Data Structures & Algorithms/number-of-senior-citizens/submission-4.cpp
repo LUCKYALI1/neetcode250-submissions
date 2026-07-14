253fd366b9f185dc309ff001ab0908da07a9aa69
Lucky Ali <luckyali786ashu@gmail.com>
2026-07-14 12:03:19 +0530

    Add: number-of-senior-citizens - submission-4

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+
+        for (string str : details) {
+            int age = (str[11] - '0') * 10 + (str[12] - '0');
+
+            if (age > 60) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
